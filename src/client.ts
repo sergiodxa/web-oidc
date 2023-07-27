1988d50aeccef81a1a9958011718e7b37055ebb0
Sergio Xalambrí <hello@sergiodxa.com>
2023-07-27 09:09:45 -0500

    Implement new methods on Client
    - Client#grant
    - Client#callbackParams
    - Client#oauhCallback
    - Client.register
    
    Refactor Client#refresh to use Client#grant

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
-import type { Issuer } from "./issuer";
+import { Issuer } from "./issuer";
+import { TokenSet } from "./token-set";
 
 export interface ClientOptions {
   clientID: string;
@@ -15,6 +16,10 @@ export class Client {
   constructor(issuer: Issuer, options: ClientOptions) {
     this.#issuer = issuer;
     this.#options = options;
+  }
+
+  get issuer() {
+    return this.#issuer;
   }
 
   authorizationUrl({
@@ -77,12 +82,14 @@ export class Client {
   }
 
   async userinfo(
-    accessToken: string,
+    accessToken: string | TokenSet,
     options: {
       method: "GET" | "POST";
       via: "header" | "body";
     } = { method: "GET", via: "header" }
   ): Promise<UserInfo> {
+    let endpoint = assert(this.#issuer, "userinfo_endpoint");
+
     let method = (options.method ?? "GET").toUpperCase();
 
     if (options.method !== "GET" && options.method !== "POST") {
@@ -93,21 +100,22 @@ export class Client {
       throw new Error("Cannot send access token in body with GET method.");
     }
 
-    let issuer = this.#issuer.metadata;
-
-    let url = new URL(issuer.userinfo_endpoint);
+    let url = new URL(endpoint);
 
     let headers = new Headers();
     let init: RequestInit = { method, headers };
 
+    let token =
+      accessToken instanceof TokenSet ? accessToken.access_token : accessToken;
+
     if (options.via === "header") {
-      headers.set("authorization", `Bearer ${accessToken}`);
+      headers.set("authorization", `Bearer ${token}`);
     }
 
     if (options.via === "body") {
       headers.set("content-type", "application/x-www-form-urlencoded");
       let body = new URLSearchParams();
-      body.append("access_token", accessToken);
+      body.append("access_token", token);
       init.body = body;
     }
 
@@ -118,29 +126,43 @@ export class Client {
     return await UserInfoSchema.promise().parse(response.json());
   }
 
-  async refresh(refreshToken: string) {
+  async refresh(refreshToken: string | TokenSet) {
     let body = new URLSearchParams({
       grant_type: "refresh_token",
-      client_id: this.#options.clientID,
-      refresh_token: refreshToken,
+      refresh_token:
+        refreshToken instanceof TokenSet
+          ? refreshToken.refresh_token
+          : refreshToken,
     });
 
+    return await this.grant(body);
+  }
+
+  async grant(body: URLSearchParams): Promise<TokenSet> {
+    let endpoint = assert(this.#issuer, "token_endpoint");
+
+    let headers: HeadersInit = {
+      accept: "application/json",
+      "content-type": "application/x-www-form-urlencoded",
+    };
+
+    body.set("client_id", this.#options.clientID);
     if (this.#options.clientSecret) {
       body.set("client_secret", this.#options.clientSecret);
     }
 
-    let issuer = this.#issuer.metadata;
-    issuer.token_endpoint;
-
-    let response = await fetch(issuer.token_endpoint, {
+    let response = await fetch(endpoint, {
       method: "POST",
-      headers: { "content-type": "application/x-www-form-urlencoded" },
+      headers,
       body,
     });
 
-    if (!response.ok) throw new Error("Failed to refresh token.");
+    if (!response.ok) {
+      let body = await response.text();
+      throw new Error("Failed to fetch token endpoint", { cause: body });
+    }
 
-    return z
+    let result = await z
       .object({
         access_token: z.string(),
         expires_in: z.literal(86400),
@@ -153,6 +175,155 @@ export class Client {
       })
       .promise()
       .parse(response.json());
+
+    return new TokenSet(result);
+  }
+
+  async callbackParams(input: string | URL | Request) {
+    let params: URLSearchParams;
+
+    if (typeof input === "string") params = new URL(input).searchParams;
+    else if (input instanceof URL) params = input.searchParams;
+    else if (input instanceof Request) {
+      let method = input.method.toUpperCase();
+      if (method === "GET") params = new URL(input.url).searchParams;
+      if (input.body === undefined) {
+        throw new TypeError("Input Request must have a body if it's not a GET");
+      }
+      let request = new Request(input.url, input);
+      params = new URLSearchParams(await request.text());
+    } else throw new TypeError("Invalid input for Client#callbackParams");
+
+    return params;
+  }
+
+  oauthCallback(
+    redirectURL: URL,
+    incoming: URLSearchParams,
+    checks: {
+      response_type?: ResponseType[];
+      state?: string;
+      nonce?: string;
+    }
+  ) {
+    let stateUrl = incoming.get("state");
+
+    if (incoming.has("state") && !checks.state) {
+      throw new TypeError("Missing Client#callback checks.state");
+    }
+
+    if (!incoming.has("state") && checks.state) {
+      throw new ReferenceError("Missing state on URL.");
+    }
+
+    if (incoming.get("state") !== checks.state) {
+      throw new RangeError("State mismatch.", {
+        cause: { expected: checks.state, actual: stateUrl },
+      });
+    }
+
+    const RESPONSE_TYPE_REQUIRED_PARAMS: Partial<
+      Record<ResponseType, string[]>
+    > = {
+      code: ["code"],
+      id_token: ["id_token"],
+      token: ["access_token", "token_type"],
+    };
+
+    for (let type of checks.response_type ?? []) {
+      if (type === "none") {
+        if (
+          incoming.has("code") ||
+          incoming.has("id_token") ||
+          incoming.has("access_token")
+        ) {
+          throw new RangeError(
+            "Unexpected parameters for response_type none.",
+            { cause: "Expected none, received code, id_token or access_token" }
+          );
+        }
+      } else if (type in RESPONSE_TYPE_REQUIRED_PARAMS) {
+        let requiredParams = RESPONSE_TYPE_REQUIRED_PARAMS[type] ?? [];
+        for (let param of requiredParams) {
+          if (!incoming.has(param)) {
+            throw new ReferenceError(
+              `Missing ${param} on URL for response_type ${type}.`
+            );
+          }
+        }
+      }
+    }
+
+    if (incoming.has("id_token")) {
+      throw new Error("id_token flow not implemented");
+    }
+
+    if (incoming.has("access_token")) {
+      throw new Error("token flow not implemented");
+    }
+
+    if (incoming.has("code")) {
+      let params = new URLSearchParams({
+        grant_type: "authorization_code",
+        code: incoming.get("code")!,
+        redirect_uri: redirectURL.toString(),
+      });
+
+      return this.grant(params);
+    }
+
+    throw new Error("No valid response_type found.");
+  }
+
+  static async register(
+    issuer: Issuer | URL | string,
+    options: {
+      initialAccessToken?: string;
+    } = {}
+  ) {
+    if (!(issuer instanceof Issuer)) {
+      issuer = await Issuer.discover(issuer);
+    }
+
+    let endpoint = assert(issuer, "registration_endpoint");
+
+    let headers = new Headers({
+      accept: "application/json",
+      "content-type": "application/json",
+    });
+
+    if (options.initialAccessToken) {
+      headers.set("authorization", `Bearer ${options.initialAccessToken}`);
+    }
+
+    let response = await fetch(endpoint, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(issuer.metadata),
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to register client for issuer ${issuer.metadata.issuer}`
+      );
+    }
+
+    let body = await z
+      .object({
+        client_id: z.string(),
+        client_secret: z.string(),
+        redirect_uri: z.string().url(),
+        response_type: ResponseTypeSchema,
+      })
+      .promise()
+      .parse(response.json());
+
+    return new Client(issuer, {
+      clientID: body.client_id,
+      clientSecret: body.client_secret,
+      redirectUri: body.redirect_uri,
+      responseType: body.response_type,
+    });
   }
 }
 
@@ -248,3 +419,16 @@ const AuthenticationRequestParamsSchema = z.object({
 type AuthenticationRequestParams = z.infer<
   typeof AuthenticationRequestParamsSchema
 >;
+
+function assert(issuer: Issuer, endpoint: keyof Issuer["metadata"]) {
+  let value = issuer.metadata[endpoint];
+
+  if (!value) {
+    throw new TypeError(`${endpoint} must be configured on the issuer`);
+  }
+  if (typeof value !== "string") {
+    throw new TypeError(`${endpoint} must be a string`);
+  }
+
+  return new URL(value);
+}
