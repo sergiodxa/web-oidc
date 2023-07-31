11df16f6cb02106d200c7192b69f812b51d74955
Sergio Xalambrí <hello@sergiodxa.com>
2023-07-31 17:36:27 -0500

    Add generator method for codeVerifier and codeChallenge

--- src/generator.ts
+++ src/generator.ts
@@ -1,8 +1,29 @@
 export class Generator {
   static state() {
+    return Generator.random();
+  }
+
+  static codeVerifier() {
+    return this.random(128);
+  }
+
+  static async codeChallenge(codeVerifier: string) {
+    let buffer = await crypto.subtle.digest(
+      "SHA-256",
+      new TextEncoder().encode(codeVerifier)
+    );
+
+    let hash = Array.from(new Uint8Array(buffer))
+      .map((byte) => byte.toString(16).padStart(2, "0"))
+      .join("");
+
+    return Buffer.from(hash).toString("base64url");
+  }
+
+  private static random(bytes = 32) {
     let result: string[] = [];
 
-    for (let value of crypto.getRandomValues(new Uint8Array(16))) {
+    for (let value of crypto.getRandomValues(new Uint8Array(bytes))) {
       result.push(value.toString(16).padStart(2, "0"));
     }
 
--- tests/generator.test.ts
+++ tests/generator.test.ts
@@ -1,11 +1,25 @@
 import { describe, expect, test } from "vitest";
 
 import { Generator } from "../src/generator";
-import { z } from "zod";
 
 describe(Generator.name, () => {
-  test("generates a random uuid", () => {
+  test("generates a random state", () => {
     let state = Generator.state();
     expect(state).toStrictEqual(expect.any(String));
+  });
+
+  test("generates a random code verifier", () => {
+    let codeVerifier = Generator.codeVerifier();
+    expect(codeVerifier).toStrictEqual(expect.any(String));
+  });
+
+  test("generates a code challenge from a code verifier", async () => {
+    let codeVerifier = "code-verifier";
+
+    let codeChallenge = await Generator.codeChallenge(codeVerifier);
+
+    expect(codeChallenge).toBe(
+      "YTlkODBiMmQxYWY1YjFhMTQ3NGZhMGQ2N2Q2NTM2ZjE1MzRmMjIyZWRlZjcyOWM0MDY3MDQ2ZGNlNjlkYzNkOA"
+    );
   });
 });
