8e4353b95a834fbff6631dc1a7fd9a3efda59765
Sergio Xalambrí <hello@sergiodxa.com>
2023-08-14 15:58:51 -0500

    Update userinfo schema

--- src/client.ts
+++ src/client.ts
@@ -386,17 +386,23 @@ const UserInfoSchema = z
 		middle_name: z.string().optional(),
 		nickname: z.string().optional(),
 		preferred_username: z.string().optional(),
-		profile: z.string().optional(),
-		picture: z.string().optional(),
-		website: z.string().optional(),
-		email: z.string().optional(),
+		profile: z.string().url().optional(),
+		picture: z.string().url().optional(),
+		website: z.string().url().optional(),
+		email: z.string().email().optional(),
 		email_verified: z.boolean().optional(),
 		gender: z.string().optional(),
 		birthdate: z.string().optional(),
 		zoneinfo: z.string().optional(),
 		locale: z.string().optional(),
 		phone_number: z.string().optional(),
-		updated_at: z.string().datetime().optional(),
+		phone_number_verified: z.boolean().optional(),
+		updated_at: z
+			.union([
+				z.string().datetime(),
+				z.number().transform((timestamp) => new Date(timestamp).toISOString()),
+			])
+			.optional(),
 		address: z
 			.object({
 				formatted: z.string().optional(),
